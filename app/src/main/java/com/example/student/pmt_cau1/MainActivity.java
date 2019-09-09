accf48647855eb78058f3cb799d77012c3cc46a7
pmt199x <pmt199x@gmail.com>
2019-09-09 16:14:23 +0700

    Commit

--- app/src/main/java/com/example/student/pmt_cau1/MainActivity.java
+++ app/src/main/java/com/example/student/pmt_cau1/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnDangNhap;
     CheckBox chkLuuThongTin;
+    TextView txtTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
